2de9d871e3cce38926ceb7db2d5f7f496888085e
K Kavyashree Karanth <168859458+KavyaKaranth@users.noreply.github.com>
2025-08-03 11:47:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function getWeather() {
   }
 
   try {
-    const apiKey = " ";
+    const apiKey = "YOUR_API_KEY ";
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     );
